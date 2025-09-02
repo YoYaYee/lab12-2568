ef05743cc0530a7d122b57f965802bf1b2b77d7f
YoungIce <EDEN555007@gmail.com>
2025-09-02 10:22:05 +0700

    Update

--- src/main.tsx
+++ src/main.tsx
@@ -8,7 +8,7 @@ import { MantineProvider, createTheme, virtualColor } from "@mantine/core";
 const theme = createTheme({
   fontFamily: "Open Sans, sans-serif",
   colors: {
-    kota: virtualColor({
+    ice: virtualColor({
       name: "ice",
       dark: "yellow",
       light: "cyan",
